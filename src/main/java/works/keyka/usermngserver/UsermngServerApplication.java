14fb66d19893be6f826c6c124806fea631de7321
keyka <tkwsm1221@gmail.com>
2025-06-07 18:52:33 +0900

    first commit

---
+++ src/main/java/works/keyka/usermngserver/UsermngServerApplication.java
@@ -0,0 +1,13 @@
+package works.keyka.usermngserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermngServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermngServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/works/keyka/usermngserver/UsermngServerApplicationTests.java
@@ -0,0 +1,13 @@
+package works.keyka.usermngserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermngServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
